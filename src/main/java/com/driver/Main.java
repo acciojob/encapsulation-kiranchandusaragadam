bfb6f08d23a93fff3724b6cb64a0dfe4156e8810
Kiranchandu Saragadam <110031953+kiranchandusaragadam@users.noreply.github.com>
2023-10-13 19:35:35 +0530

    Encapsulation done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,13 @@
 package com.driver;
 
 public class Main {
-  RWOnly obj = new RWOnly();
-  obj.setName("Kiran);
-  System.out.print(obj.getName);
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+
+    obj.setName("Encapsulation");
+
+    String s = obj.getName();
+
+    System.out.println(s);
+  }
 }
